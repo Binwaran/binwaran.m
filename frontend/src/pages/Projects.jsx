e40ba5ef588dd7b9c13e1e023f3d8c2d0cd59172
Binwaran <binwaran.m@gmail.com>
2025-03-18 16:40:39 +0800

    Add: projects page

---
+++ frontend/src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+const Projects = () => {
+    return (
+      <div>
+        <h1>Project Page</h1>
+        <p>Welcome to my personal portfolio.</p>
+      </div>
+    );
+  };
+  
+  export default Projects;

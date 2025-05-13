eda96907848a4cc9d0023b1e38344231504e471f
Binwaran <binwaran.m@gmail.com>
2025-05-13 21:32:21 +0800

    feat: create basic Express API for /profiles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,6 @@ import NotFound from "./pages/NotFound";
 
 
 
-
 function App() {
   return (
     <Router> {/* Router ต้องเป็นตัวครอบหลัก */}
---
+++ personal-blog-api/app.mjs
@@ -0,0 +1,29 @@
+// app.mjs
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Route สำหรับดูโปรไฟล์ John
+app.get("/profiles", (req, res) => {
+  res.status(200).json({
+    data: {
+      name: "john",
+      age: 20,
+    },
+  });
+});
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("Personal Blog API is running!");
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server is running at http://localhost:${port}`);
+});
